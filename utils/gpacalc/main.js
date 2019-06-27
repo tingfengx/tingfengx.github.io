c786c719c0da2d2979bd19910d23c294a6f22f5a
TINGFENG XIA <tingfeng.xia@outlook.com>
2019-06-27 14:46:26 +0800

    revisit gpa rule

--- utils/gpacalc/main.js
+++ utils/gpacalc/main.js
@@ -23,27 +23,27 @@ var app = new Vue({
         },
 
         compute_one_gpa: function (mark) {
-            if (mark > 85) {
+            if (mark >= 85) {
                 return 4.0;
-            } else if (mark > 80) {
+            } else if (mark >= 80) {
                 return 3.7;
-            } else if (mark > 77) {
+            } else if (mark >= 77) {
                 return 3.3;
-            } else if (mark > 73) {
+            } else if (mark >= 73) {
                 return 3.0;
-            } else if (mark > 70) {
+            } else if (mark >= 70) {
                 return 2.7;
-            } else if (mark > 67) {
+            } else if (mark >= 67) {
                 return 2.3;
-            } else if (mark > 63) {
+            } else if (mark >= 63) {
                 return 2.0;
-            } else if (mark > 60) {
+            } else if (mark >= 60) {
                 return 1.7;
-            } else if (mark > 57) {
+            } else if (mark >= 57) {
                 return 1.3;
-            } else if (mark > 53) {
+            } else if (mark >= 53) {
                 return 1.0;
-            } else if (mark > 50) {
+            } else if (mark >= 50) {
                 return 0.7;
             } else {
                 return 0.0;
