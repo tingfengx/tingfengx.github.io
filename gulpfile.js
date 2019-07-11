1068d1205498c0a52cccc48fe62618e3dfcad28c
TINGFENG XIA <tingfeng.xia@outlook.com>
2019-07-11 14:45:47 +0800

    styling imporve

--- gulpfile.js
+++ gulpfile.js
@@ -63,9 +63,9 @@ function modules() {
     .pipe(gulp.dest('./vendor/jquery-easing'));
   // jQuery
   var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
     .pipe(gulp.dest('./vendor/jquery'));
   // Simple Line Icons
   var simpleLineIconsFonts = gulp.src('./node_modules/simple-line-icons/fonts/**')
